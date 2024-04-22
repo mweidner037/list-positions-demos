21aebf99016106fb96320612cefb7b0fb8c02e4e
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2024-04-22 13:57:32 -0400

    copy websocket-prosemirror

---
+++ websocket-prosemirror-2/src/common/block_text.ts
@@ -0,0 +1,22 @@
+import { TimestampFormattingSavedState } from "list-formatting";
+import { ListSavedState, OrderSavedState } from "list-positions";
+
+/**
+ * Immutable - don't mutate attrs directly.
+ */
+export type BlockMarker = {
+  readonly type: string;
+  readonly attrs?: Record<string, any>;
+  /**
+   * Lamport timestamp for LWW.
+   */
+  readonly timestamp: number;
+  readonly creatorID: string;
+};
+
+export type BlockTextSavedState = {
+  readonly order: OrderSavedState;
+  readonly text: ListSavedState<string>;
+  readonly blockMarkers: ListSavedState<BlockMarker>;
+  readonly formatting: TimestampFormattingSavedState;
+};
